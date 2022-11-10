a47246c91bca102b2d5116df4233141b0eb8fef4
AlvaroGabriell <114313448+AlvaroGabriell@users.noreply.github.com>
2022-11-10 09:39:02 -0300

    att

---
+++ script.js
@@ -0,0 +1,6 @@
+function oneClickMenu() {
+    document.querySelector("#icon").classList.toggle("change")
+    document.querySelector("#close").classList.toggle("change")
+   
+    document.querySelector(".nav").classList.toggle("change")
+}
